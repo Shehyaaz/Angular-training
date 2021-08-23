1c685bdb0d2d75b17a6195f1d66bc5e1803a1cae
Shehyaaz <shehyaaz.cs17@bmsce.ac.in>
2021-08-24 00:04:20 +0530

    completed assignment-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .paragraphColor{
+      color: white;
+    }
+  `]
 })
-export class AppComponent { }
+export class AppComponent { 
+  display = true;
+  btnClicks = [];
+
+  onButtonClick(){
+    this.display = !this.display;
+    this.btnClicks.push(this.btnClicks.length + 1);
+  }
+}
