784650275601a27cb32911d18a0660e790f16cc5
Shehyaaz <shehyaaz.cs17@bmsce.ac.in>
2021-08-24 18:24:37 +0530

    added recipe list and recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "This is simply a test recipe", 
+    "https://thumbs.dreamstime.com/b/recipe-word-text-green-leaf-handwritten-lettering-recipe-word-text-green-leaf-handwritten-lettering-suitable-112679873.jpg")
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,8 @@
+// this class defines a structure of a single recipe
+export class Recipe {
+    constructor(
+        public name: string,
+        public description: string,
+        public imagePath: string
+    ) { }
+}
