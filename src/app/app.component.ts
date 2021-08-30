d8f25e4bf46ce812754b706daab97d59f37b4491
Shehyaaz <shehyaaz.cs17@bmsce.ac.in>
2021-08-31 00:55:02 +0530

    completed assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UsersService]
 })
 export class AppComponent { }
